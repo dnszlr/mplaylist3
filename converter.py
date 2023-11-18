3a8d5ecd83105156081ffb6ba73461b7ae8fb0b7
dns <zede1011@h-ka.de>
2023-11-18 17:07:49 +0100

    ui

--- converter.py
+++ converter.py
@@ -2,26 +2,23 @@ import os
 from datetime import datetime
 from moviepy.editor import AudioFileClip
 
-def convert_to_mp3(audio_data, playlist_title):
+def convert_to_mp3(data, playlist_title):
     output_folder = os.path.join("./out", playlist_title)
     os.makedirs(output_folder, exist_ok=True)
-    for idx, data in enumerate(audio_data, start=1):
-        try:
-            # Download as mp4
-            title = replace_forbidden_characters(data['title'] + ".mp4")
-            print(f"Title: {title}")
-            data['stream'].download(filename = title, output_path = output_folder)
-            print(f"Downloaded MP4 {idx}/{len(audio_data)}")
-            mp4 = os.path.join(output_folder, title)
-            mp3 = os.path.join(output_folder, title[:-1] + '3')
-            # Convert to mp3
-            audio_clip = AudioFileClip(mp4)
-            audio_clip.write_audiofile(mp3)
-            audio_clip.close()
-            os.remove(mp4)
-            print(f"Converted MP3 {idx}/{len(audio_data)}")
-        except Exception as err:
-            print(f"Error while downloading or converting {data['title']} to mp3 with {err}")
+    try:
+        # Download as mp4
+        title = replace_forbidden_characters(data['title'] + ".mp4")
+        print(f"Downloading Title: {title}")
+        data['stream'].download(filename = title, output_path = output_folder)
+        mp4 = os.path.join(output_folder, title)
+        mp3 = os.path.join(output_folder, title[:-1] + '3')
+        # Convert to mp3
+        audio_clip = AudioFileClip(mp4)
+        audio_clip.write_audiofile(mp3)
+        audio_clip.close()
+        os.remove(mp4)
+    except Exception as err:
+        print(f"Inner error while downloading or converting {data['title']} to mp3 with {err}")
 
 
 def replace_forbidden_characters(filename):
--- mpl3.py
+++ mpl3.py
@@ -1,19 +1,98 @@
 import sys
+import threading
 from converter import convert_to_mp3
 from playlist_parser import get_playlist, get_videos, get_video_streams
+import tkinter as tk
+from tkinter import messagebox, ttk
 
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <playlist_url>")
-        sys.exit(1)
-    
-    playlist_url = sys.argv[1]
-    playlist = get_playlist(str(playlist_url))
-    playlist_title = playlist.title
-    videos = get_videos(playlist)
-    streams = get_video_streams(videos)
-    convert_to_mp3(streams, playlist_title)
+class MPL3:
+    def __init__(self, master):
+
+        # Set up master
+        self.master = master
+        self.master.title("MPL3 YouTube Playlist Downloader")
+        self.master.geometry("800x600")
+
+        # Data storage
+        self.videos = []
+
+        # Playlist Link Entry
+        self.playlist_label = tk.Label(master, text="Enter Playlist URL")
+        self.playlist_label.pack()
+
+        self.playlist_entry = tk.Entry(master)
+        self.playlist_entry.pack(fill=tk.X)
+
+        # Playlist Listbox
+        self.playlist_label = tk.Label(master, text="Playlist")
+        self.playlist_label.pack()
+
+        self.playlist_listbox = tk.Listbox(master, selectmode=tk.MULTIPLE)
+        self.playlist_listbox.pack(expand=True, fill=tk.BOTH)
+
+        self.progress_var = tk.DoubleVar()
+        self.progress_bar = ttk.Progressbar(root, variable=self.progress_var)
+        self.progress_bar.pack(expand=True, fill=tk.BOTH)
+
+        # Preview Button
+        self.preview_button = tk.Button(master, text="Preview", width=15, command=self.preview_playlist)
+        # Download Button
+        self.download_button = tk.Button(master, text="Download", width=15, command=self.start_download)
+
+        # Use the grid geometry manager to center the buttons
+        self.preview_button.pack(side='left', anchor='e', expand=True)
+        self.download_button.pack(side='right', anchor='w', expand=True)
+
+    def preview_playlist(self):
+        playlist_url = self.playlist_entry.get()
+        if playlist_url:
+            print(f"Playlist url: {playlist_url}")
+            playlist = get_playlist(str(playlist_url))
+            self.playlist_label.config(text=playlist.title)
+            videos = get_videos(playlist)
+            for video in videos:
+                title = video.title
+                self.videos.append(video)
+                self.playlist_listbox.insert(tk.END, title)
+        else:
+            messagebox.showinfo("Missing Playlist", "Please enter a Playlist URL.")
+
+    def download_playlist(self):
+        self.increase_process_value(1)
+        selected_titles = [self.playlist_listbox.get(idx) for idx in self.playlist_listbox.curselection()]
+        if len(selected_titles) == 0:
+            selected_titles = self.get_selected_titles()
+        if len(selected_titles) == 0:
+            self.preview_playlist()
+            selected_titles = self.get_selected_titles();
+        selected_videos = [video for video in self.videos if video.title in selected_titles]
+        streams = get_video_streams(selected_videos)
+        playlist_title = self.playlist_label.cget("text")
+        for idx, stream in enumerate(streams, start=1):
+            print(f"Steam is: {stream}")
+            try:
+                convert_to_mp3(stream, playlist_title)
+            except Exception as err:
+                print(f"Outer error while downloading or converting {stream['title']} to mp3 with {err}")
+                        # Update progress bar value based on the current iteration
+            self.increase_process_value((idx / len(streams)) * 100)
+        messagebox.showinfo("Done", "Your files have been successfully downloaded!")
+
+    def get_selected_titles(self):
+        return [self.playlist_listbox.get(idx) for idx in range(self.playlist_listbox.size())]
+
+
+    def start_download(self):
+        # Start the download task in a separate thread
+        download_thread = threading.Thread(target=self.download_playlist)
+        download_thread.start()
+
+    def increase_process_value(self, amount):
+        progress_value = amount
+        self.progress_var.set(progress_value)
+        root.update_idletasks()
 
 if __name__ == "__main__":
-    main()
-
+    root = tk.Tk()
+    app = MPL3(root)
+    root.mainloop()
