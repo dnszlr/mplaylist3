d1b8fede9256f4882d5b8f351e70465a0b3f8e8d
dns <zede1011@h-ka.de>
2023-11-18 14:50:34 +0100

    fixes

--- playlist_parser.py
+++ playlist_parser.py
@@ -2,9 +2,13 @@ import re
 from pytube import Playlist, YouTube 
 
 def get_playlist(playlist_url):
-    print(f"Playlist URL: {playlist_url}")
-    playlist = Playlist(playlist_url)
-    playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+    playlist = None
+    try:
+        print(f"Playlist URL: {playlist_url}")
+        playlist = Playlist(playlist_url)
+        playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+    except Exception as err:
+        print(f"Invalid playlist URL: {err}")
     return playlist
 
 def get_videos(playlist):
@@ -14,17 +18,20 @@ def get_videos(playlist):
         for video in playlist.videos:
             videos.append(video)
     else:
-        print("Invalid playlist URL")    
+        print("Invalid playlist")    
     return videos
 
 def get_video_streams(videos):
     video_streams = []
     print("Process videos streams")
     for video in videos:
-        print(f": {video.title}")
-        video_data = {
-            'title': video.title,
-            'stream': video.streams.get_highest_resolution()
-        }
-        video_streams.append(video_data)
+        try:
+            print(f": {video.title}")
+            video_data = {
+                'title': video.title,
+                'stream': video.streams.get_highest_resolution()
+            }
+            video_streams.append(video_data)
+        except Exception as err:
+            print(f"Exception occured {err}")
     return video_streams
