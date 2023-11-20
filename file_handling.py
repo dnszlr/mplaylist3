a144e2a630e0813b2f86cd48874607a29c4c834a
dns <zede1011@h-ka.de>
2023-11-20 19:43:24 +0100

    path fixes

--- file_handling.py
+++ file_handling.py
@@ -2,9 +2,11 @@ import os, sys
 
 def get_root_folder():
     path = os.path.abspath(__file__)
-    if is_exe:
+    if is_exe():
+        print("is exe")
         path = sys.executable
-    return os.path.dirname(path)
+    dire = os.path.dirname(path)
+    return dire
 
 def is_exe():
     return getattr(sys, 'frozen', False)
